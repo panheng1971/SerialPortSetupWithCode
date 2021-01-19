c19f0f34e860e83a7c2dac59716473708ff00cd2
panheng1971 <pan.heng@hotmail.com>
2021-01-19 10:34:12 +0800

    modified by panheng 2021-01-19

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonOpenPorts = new System.Windows.Forms.Button();
+            this.buttonSendData = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonOpenPorts
+            // 
+            this.buttonOpenPorts.Location = new System.Drawing.Point(55, 37);
+            this.buttonOpenPorts.Name = "buttonOpenPorts";
+            this.buttonOpenPorts.Size = new System.Drawing.Size(158, 50);
+            this.buttonOpenPorts.TabIndex = 0;
+            this.buttonOpenPorts.Text = "open all ports";
+            this.buttonOpenPorts.UseVisualStyleBackColor = true;
+            this.buttonOpenPorts.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // buttonSendData
+            // 
+            this.buttonSendData.Location = new System.Drawing.Point(55, 102);
+            this.buttonSendData.Name = "buttonSendData";
+            this.buttonSendData.Size = new System.Drawing.Size(158, 53);
+            this.buttonSendData.TabIndex = 1;
+            this.buttonSendData.Text = "send data to all ports";
+            this.buttonSendData.UseVisualStyleBackColor = true;
+            this.buttonSendData.Click += new System.EventHandler(this.buttonSendData_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.buttonSendData);
+            this.Controls.Add(this.buttonOpenPorts);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonOpenPorts;
+        private System.Windows.Forms.Button buttonSendData;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+namespace WindowsFormsApp1
+{
+
+    public partial class Form1 : Form
+    {
+
+        SerialPort[] SerialPorts;
+
+        string[] SerialPortNames = SerialPort.GetPortNames();
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            SerialPortNames = SerialPort.GetPortNames();
+            SerialPorts = new SerialPort[SerialPortNames.Length];
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int num = 0; num < SerialPortNames.Length; num++)
+            {
+
+             
+                SerialPorts[num] = new SerialPort();
+                SerialPorts[num].DataReceived += new SerialDataReceivedEventHandler(recievedata);//绑定事件
+                SerialPorts[num].PortName = SerialPortNames[num];
+                SerialPorts[num].BaudRate = 115200;
+                SerialPorts[num].Parity = Parity.None;
+                SerialPorts[num].DataBits = 8;
+                SerialPorts[num].StopBits = StopBits.One;
+                SerialPorts[num].Open();
+            }
+        }
+
+
+
+
+
+
+        private byte[] strToHexByte(string hexString)             //字符串转换为byte[]
+        {
+            hexString = hexString.Replace(" ", "");
+            if ((hexString.Length % 2) != 0) hexString += " ";
+            byte[] returnBytes = new byte[hexString.Length / 2];
+            for (int i = 0; i < returnBytes.Length; i++)
+                returnBytes[i] = Convert.ToByte(hexString.Substring(i * 2, 2).Replace(" ", ""), 16);
+            return returnBytes;
+        }
+
+
+
+        public void recievedata(object sender, SerialDataReceivedEventArgs e)                 //接收读取数据
+        {
+
+            byte[] ReDatas = new byte[((SerialPort)sender).BytesToRead];
+            ((SerialPort)sender).Read(ReDatas, 0, ReDatas.Length);//读取数据
+
+            MessageBox.Show("serial port "+ ((SerialPort)sender).PortName.ToString()+" recieved data: "+ReDatas[0].ToString());
+
+        }
+
+        private void buttonSendData_Click(object sender, EventArgs e)
+        {
+            byte[] data = { 0x9 };
+          
+            for (int num = 0; num < SerialPortNames.Length; num++)
+            {
+                if (SerialPorts[num].IsOpen)
+                {
+                    try
+                    {
+                        SerialPorts[num].Write(data, 0, data.Length);//发送数据
+
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message, "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("串口未打开", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+
+        }
+    }
+}
+    
+
+
+    
+
